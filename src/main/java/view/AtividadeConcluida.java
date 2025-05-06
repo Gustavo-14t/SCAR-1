08435ca0a08846c2bb9d5c4182f74baad451db1e
Gustavo <garamujo07@gmail.com>
2025-05-06 12:02:58 -0300

    Listando Atividades Pendentes e Concluídas, próximo passo, Baixar relatório de atividades concluídas

---
+++ src/main/java/view/AtividadeConcluida.java
@@ -0,0 +1,277 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package view;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Insets;
+
+/**
+ *
+ * @author Todosdacasa
+ */
+public class AtividadeConcluida extends javax.swing.JFrame {
+
+    /**
+     * Creates new form AtividadeConcluida
+     */
+    public AtividadeConcluida() {
+        initComponents();
+        
+        painelAtividades.setLayout(new javax.swing.BoxLayout(painelAtividades, javax.swing.BoxLayout.Y_AXIS));
+
+// Envolva com JScrollPane
+javax.swing.JScrollPane scrollPane = new javax.swing.JScrollPane(painelAtividades);
+scrollPane.setVerticalScrollBarPolicy(javax.swing.JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+scrollPane.setHorizontalScrollBarPolicy(javax.swing.JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+getContentPane().add(scrollPane, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 720, 410));
+
+listarAtividadesNaTela();
+    }
+    
+    private model.Atividade atividadeSelecionada;
+
+    
+    public class WrapLayout extends FlowLayout {
+    public WrapLayout() {
+        super();
+    }
+
+    public WrapLayout(int align) {
+        super(align);
+    }
+
+    public WrapLayout(int align, int hgap, int vgap) {
+        super(align, hgap, vgap);
+    }
+
+    @Override
+    public Dimension preferredLayoutSize(Container target) {
+        return layoutSize(target, true);
+    }
+
+    @Override
+    public Dimension minimumLayoutSize(Container target) {
+        Dimension minimum = layoutSize(target, false);
+        minimum.width -= (getHgap() + 1);
+        return minimum;
+    }
+
+    private Dimension layoutSize(Container target, boolean preferred) {
+        synchronized (target.getTreeLock()) {
+            int targetWidth = target.getWidth();
+
+            if (targetWidth == 0) {
+                targetWidth = Integer.MAX_VALUE;
+            }
+
+            int hgap = getHgap();
+            int vgap = getVgap();
+            Insets insets = target.getInsets();
+            int maxWidth = targetWidth - (insets.left + insets.right + hgap * 2);
+
+            Dimension dim = new Dimension(0, 0);
+            int rowWidth = 0;
+            int rowHeight = 0;
+
+            int nmembers = target.getComponentCount();
+
+            for (int i = 0; i < nmembers; i++) {
+                Component m = target.getComponent(i);
+
+                if (m.isVisible()) {
+                    Dimension d = preferred ? m.getPreferredSize() : m.getMinimumSize();
+
+                    if (rowWidth + d.width > maxWidth) {
+                        addRow(dim, rowWidth, rowHeight);
+                        rowWidth = 0;
+                        rowHeight = 0;
+                    }
+
+                    if (rowWidth != 0) {
+                        rowWidth += hgap;
+                    }
+
+                    rowWidth += d.width;
+                    rowHeight = Math.max(rowHeight, d.height);
+                }
+            }
+
+            addRow(dim, rowWidth, rowHeight);
+
+            dim.width += insets.left + insets.right + hgap * 2;
+            dim.height += insets.top + insets.bottom + vgap * 2;
+
+            return dim;
+        }
+    }
+
+    private void addRow(Dimension dim, int rowWidth, int rowHeight) {
+        dim.width = Math.max(dim.width, rowWidth);
+
+        if (dim.height > 0) {
+            dim.height += getVgap();
+        }
+
+        dim.height += rowHeight;
+    }
+}
+    
+    private void listarAtividadesNaTela() {
+    painelAtividades.removeAll(); // limpa antes de atualizar
+
+    
+    
+    controller.AtividadeController controller = new controller.AtividadeController();
+    java.util.List<model.Atividade> lista = controller.listarAtividadeConc(); // chama o DAO
+
+    if (lista != null) {
+        for (model.Atividade a : lista) {
+            javax.swing.JEditorPane area = new javax.swing.JEditorPane();
+            area.setFont(new java.awt.Font("Dialog", java.awt.Font.PLAIN, 14)); // ou BOLD, ou 16
+
+            area.addMouseListener(new java.awt.event.MouseAdapter() {
+    @Override
+    public void mouseClicked(java.awt.event.MouseEvent evt) {
+        
+      atividadeSelecionada = a; // guarda qual foi clicada
+    }
+});
+            
+area.setPreferredSize(new java.awt.Dimension(680, 100));
+area.setContentType("text/html");
+area.setText(
+    "<html>" +
+    "<b>Nome:</b> " + a.getNome() + "<br>" +
+    "<b>Descrição:</b> " + a.getDescricao() + "<br>" +
+    "<b>Prazo:</b> " + a.getDatas() + "<br>" +
+    "<b>Status:</b> " + a.getStatu() +
+    "</html>"
+);
+area.setEditable(false);
+area.setBackground(new java.awt.Color(255, 255, 204));
+area.setBorder(javax.swing.BorderFactory.createCompoundBorder(
+    javax.swing.BorderFactory.createLineBorder(java.awt.Color.GRAY),
+    javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10)
+));
+
+ area.addMouseListener(new java.awt.event.MouseAdapter() {
+                @Override
+                public void mouseClicked(java.awt.event.MouseEvent evt) {
+                    atividadeSelecionada = a;
+                }
+            });
+
+            painelAtividades.add(area);
+            painelAtividades.add(javax.swing.Box.createVerticalStrut(10));
+        }
+
+        painelAtividades.revalidate();
+        painelAtividades.repaint();
+    }
+    }
+
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        painelAtividades = new javax.swing.JPanel();
+        painelBar = new javax.swing.JPanel();
+        bBaixarRela = new javax.swing.JToggleButton();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        painelAtividades.setBackground(new java.awt.Color(255, 204, 0));
+
+        javax.swing.GroupLayout painelAtividadesLayout = new javax.swing.GroupLayout(painelAtividades);
+        painelAtividades.setLayout(painelAtividadesLayout);
+        painelAtividadesLayout.setHorizontalGroup(
+            painelAtividadesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 720, Short.MAX_VALUE)
+        );
+        painelAtividadesLayout.setVerticalGroup(
+            painelAtividadesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(painelAtividades, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 720, 400));
+
+        painelBar.setBackground(new java.awt.Color(255, 153, 0));
+        painelBar.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        bBaixarRela.setText("Baixar Relatório");
+        painelBar.add(bBaixarRela, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 11, 140, 38));
+
+        jButton1.setText("Voltar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        painelBar.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 99, 40));
+
+        getContentPane().add(painelBar, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 400, 720, 60));
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(AtividadeConcluida.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(AtividadeConcluida.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(AtividadeConcluida.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(AtividadeConcluida.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new AtividadeConcluida().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToggleButton bBaixarRela;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel painelAtividades;
+    private javax.swing.JPanel painelBar;
+    // End of variables declaration//GEN-END:variables
+}
