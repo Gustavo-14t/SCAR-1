fc9f2a03884138f01783de4dacf1fc0e368e8d68
G <garamujo07@gmail.com>
2025-05-23 14:40:10 -0300

    transforando a tabela inoutMorador e uma tabela de vizualização e controle

--- src/main/java/model/INOUTmorador.java
+++ src/main/java/model/INOUTmorador.java
@@ -12,22 +12,16 @@ public class INOUTmorador {
     
    String id_morEntradaSaida;
    String id_morador;
-   int id_funcionario;
-   String datas;
    String statu ;
 
-    public INOUTmorador(String id_morEntradaSaida, String id_morador, int id_funcionario, String datas, String statu) {
+    public INOUTmorador(String id_morEntradaSaida, String id_morador, int id_funcionario, String statu) {
         this.id_morEntradaSaida = id_morEntradaSaida;
         this.id_morador = id_morador;
-        this.id_funcionario = id_funcionario;
-        this.datas = datas;
         this.statu = statu;
     }
 
     public INOUTmorador(String id_morador, int id_funcionario, String datas, String statu) {
         this.id_morador = id_morador;
-        this.id_funcionario = id_funcionario;
-        this.datas = datas;
         this.statu = statu;
     }
 
@@ -48,22 +42,6 @@ public class INOUTmorador {
 
     public void setId_morador(String id_morador) {
         this.id_morador = id_morador;
-    }
-
-    public int getId_funcionario() {
-        return id_funcionario;
-    }
-
-    public void setId_funcionario(int id_funcionario) {
-        this.id_funcionario = id_funcionario;
-    }
-
-    public String getDatas() {
-        return datas;
-    }
-
-    public void setDatas(String datas) {
-        this.datas = datas;
     }
 
     public String getStatu() {
